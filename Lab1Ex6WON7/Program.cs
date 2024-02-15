ad7d44e1298eaa7eee84e10367c79512f2b23117
codri <codri@VIRTUAL>
2024-02-15 22:46:04 +0200

    Add project files.

---
+++ Lab1Ex6WON7/Program.cs
@@ -0,0 +1,6 @@
+﻿/*Ex 6
+Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+impar
+ In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+ Google :even and odd numbe*/
+CheckParity(ReadNumber());
---
+++ Lab1Ex6WON7/ProgramHelpers.cs
@@ -0,0 +1,20 @@
+﻿partial class Program
+{
+    static int ReadNumber()
+    {
+        Console.WriteLine("Introducetu un numar pentru a i se verifica paritatea");
+        if(!int.TryParse(Console.ReadLine(), out var number))
+        {
+            Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+            return ReadNumber();
+        }
+        return number;
+    }
+    static void CheckParity(int numar)
+    {
+        if (numar % 2 == 0)
+            Console.WriteLine("par");
+        else
+            Console.WriteLine("impar");
+    }
+}
